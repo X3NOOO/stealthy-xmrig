15af2fffecbcbcff9d18649b5c9641fd3b31bc74
X3NO <X3NO@disroot.org>
2024-10-29 22:46:57 +0100

    Added basic watchdog

--- src/stealth_patch/watchdog/windows.cpp
+++ src/stealth_patch/watchdog/windows.cpp
@@ -1,14 +1,46 @@
 #include "detection_state.h"
 #include "windows/exit_processes.h"
 #include "windows/pause_processes.h"
-#include <iostream>
+#include <windows.h>
+#include <algorithm>
+#include <cctype>
 
 detection_state check()
 {
-    for (int i = 0; i < PAUSE_PROCESSES_COUNT; i++)
-    {
-        std::cout << "Checking for " << pause_processes[i] << std::endl;
-    }
+    detection_state state = RUN;
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE || hSnapshot == NULL)
+        return ERROR;
 
-    return RUN;
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(PROCESSENTRY32);
+
+    Process32First(hSnapshot, &pe);
+    do
+    {
+        char *process_name = new char[strlen(pe.szExeFile) + 1];
+        std::transform(pe.szExeFile, pe.szExeFile + strlen(pe.szExeFile), process_name, ::tolower);
+
+        for (const char *exit_process : exit_processes)
+        {
+            if (strcmp(process_name, exit_process) == 0)
+            {
+                state = EXIT;
+                goto exit;
+            }
+        }
+
+        for (const char *pause_process : pause_processes)
+        {
+            if (strcmp(process_name, pause_process) == 0)
+            {
+                state = PAUSE;
+                goto exit;
+            }
+        }
+    } while (Process32Next(hSnapshot, &pe));
+
+exit:
+    CloseHandle(hSnapshot);
+    return state;
 }
